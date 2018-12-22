95c0d36c512911509a8ec1ed1937790fa32899cd
ladyada <limor@ladyada.net>
2018-12-21 21:13:01 -0500

    linted

--- adafruit_framebuf.py
+++ adafruit_framebuf.py
@@ -228,7 +228,7 @@ class FrameBuffer:
     def text(self, string, x, y, color, *,
              font_name="font5x8.bin"):
         """text is not yet implemented"""
-        if not self._font or self._font._font_name != font_name:
+        if not self._font or self._font.font_name != font_name:
             # load the font!
             self._font = BitmapFont()
         w = self._font.font_width
@@ -241,6 +241,9 @@ class FrameBuffer:
 # Author: Tony DiCola
 # License: MIT License (https://opensource.org/licenses/MIT)
 class BitmapFont:
+    """A helper class to read binary font tiles and 'seek' through them as a
+    file to display in a framebuffer. We use file access so we dont waste 1KB
+    of RAM on a font!"""
     def __init__(self, font_name='font5x8.bin'):
         # Specify the drawing area width and height, and the pixel function to
         # call when drawing pixels (should take an x and y param at least).
@@ -252,47 +255,51 @@ class BitmapFont:
         # - x bytes: font data, in ASCII order covering all 255 characters.
         #            Each character should have a byte for each pixel column of
         #            data (i.e. a 5x8 font has 5 bytes per character).
-        self._font_name = font_name
+        self.font_name = font_name
 
         # Open the font file and grab the character width and height values.
         # Note that only fonts up to 8 pixels tall are currently supported.
         try:
-            self._font = open(self._font_name, 'rb')
+            self._font = open(self.font_name, 'rb')
         except OSError:
             print("Could not find font file", font_name)
             raise
         self.font_width, self.font_height = struct.unpack('BB', self._font.read(2))
 
     def deinit(self):
-        # Close the font file as cleanup.
+        """Close the font file as cleanup."""
         self._font.close()
 
     def __enter__(self):
+        """Initialize/open the font file"""
         self.__init__()
         return self
 
     def __exit__(self, exception_type, exception_value, traceback):
+        """cleanup on exit"""
         self.deinit()
 
-    def draw_char(self, ch, x, y, fb, color):
+    def draw_char(self, char, x, y, framebuffer, color):
+        # pylint: disable=too-many-arguments
+        """Draw one character at position (x,y) to a framebuffer in a given color"""
         # Don't draw the character if it will be clipped off the visible area.
-        if x < -self.font_width or x >= fb.width or \
-           y < -self.font_height or y >= fb.height:
-              return
+        if x < -self.font_width or x >= framebuffer.width or \
+           y < -self.font_height or y >= framebuffer.height:
+            return
         # Go through each column of the character.
         for char_x in range(self.font_width):
             # Grab the byte for the current column of font data.
-            self._font.seek(2 + (ord(ch) * self.font_width) + char_x)
+            self._font.seek(2 + (ord(char) * self.font_width) + char_x)
             line = struct.unpack('B', self._font.read(1))[0]
             # Go through each row in the column byte.
             for char_y in range(self.font_height):
                 # Draw a pixel for each bit that's flipped on.
                 if (line >> char_y) & 0x1:
-                    fb.pixel(x + char_x, y + char_y, color)
+                    framebuffer.pixel(x + char_x, y + char_y, color)
 
     def width(self, text):
-        # Return the pixel width of the specified text message.
-        return len(text) * (self._font_width + 1)
+        """Return the pixel width of the specified text message."""
+        return len(text) * (self.font_width + 1)
 
 
 class FrameBuffer1(FrameBuffer):  # pylint: disable=abstract-method
